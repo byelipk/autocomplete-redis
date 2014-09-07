275ad931d92560d84866d1c94d809f7140689b0c
byelipk <byelipk@gmail.com>
2014-09-07 13:05:48 -0400

    popup redesigned

--- app/assets/javascripts/lib/discovery_list.js
+++ app/assets/javascripts/lib/discovery_list.js
@@ -12,83 +12,116 @@ AutocompleteRedis.DiscoveryList = {
   },
 
   factFinder: {
-    flag: 'FF',
-    type: 'Question',
-    name: 'Fact Finder',
-    nameLower: 'fact finder',
-    shortDesc: 'Fact Finders attempt to establish verifiable facts.',
-    miniDesc: 'A question that attempts to determine basic and relatively straightforward information'
+    flag: "FF",
+    type: "Question",
+    name: "Fact Finder",
+    avatar: "avatar avatar-color-13 pull-left",
+    char: "?",
+    nameLower: "fact finder",
+    shortDesc: "Fact Finders attempt to establish verifiable facts.",
+    hiddenDesc: "A question that attempts to determine basic...",
+    miniDesc: "A question that attempts to determine basic and relatively straightforward information"
   },
 
   bridgeBuilder: {
-    flag: 'BB',
-    type: 'Question',
-    name: 'Bridge Builder',
-    nameLower: 'bridge builder',
-    shortDesc: 'Bridge Builders connect different ideas together.',
-    miniDesc: 'A question that attempts to organize facts and ideas to determine the relationship among them'
+    flag: "BB",
+    type: "Question",
+    name: "Bridge Builder",
+    avatar: "avatar avatar-color-13 pull-left",
+    // avatar: "glyphicon glyphicon-question-sign pull-left",
+    char: "?",
+    nameLower: "bridge builder",
+    shortDesc: "Bridge Builders connect different ideas together.",
+    hiddenDesc: "A question that tries to organize facts...",
+    miniDesc: "A question that attempts to organize facts and ideas to determine the relationship among them"
   },
 
   analyzer: {
-    flag: 'AN',
-    type: 'Question',
-    name: 'Analyzer',
-    nameLower: 'analyzer',
-    shortDesc: 'Analyzers expose the hidden logic of a belief.',
-    miniDesc: 'A question that tries to break a situation into its component parts and clarify its inner logic'
+    flag: "AN",
+    type: "Question",
+    name: "Analyzer",
+    avatar: "avatar avatar-color-13 pull-left",
+    // avatar: "glyphicon glyphicon-question-sign pull-left",
+    char: "?",
+    nameLower: "analyzer",
+    shortDesc: "Analyzers expose the hidden logic of a belief.",
+    hiddenDesc: "A question that tries to break down a situation...",
+    miniDesc: "A question that tries to break down a situation into its component parts and clarify its inner logic"
   },
 
   detective: {
-    flag: 'DT',
-    type: 'Question',
-    name: 'Detective',
-    nameLower: 'detective',
-    shortDesc: 'Detectives identify important missing information.',
-    miniDesc: 'A question that combines ideas to help create new solutions or make inferences regarding the future'
+    flag: "DT",
+    type: "Question",
+    name: "Detective",
+    avatar: "avatar avatar-color-13 pull-left",
+    char: "?",
+    nameLower: "detective",
+    shortDesc: "Detectives identify important missing information.",
+    hiddenDesc: "A question that combines ideas to create...",
+    miniDesc: "A question that combines ideas to help create new solutions or make inferences regarding the future"
   },
 
   messenger: {
-    flag: 'MS',
-    type: 'Question',
-    name: 'Messenger',
-    nameLower: 'messenger',
-    shortDesc: 'Messengers clarify the criteria used to render a judgement.',
-    miniDesc: 'A question that clarifies the reasoning behind a judgement or evaluation made based on certain criteria'
+    flag: "MS",
+    type: "Question",
+    name: "Messenger",
+    avatar: "avatar avatar-color-13 pull-left",
+    char: "?",
+    nameLower: "messenger",
+    shortDesc: "Messengers clarify the criteria used to render a judgement.",
+    hiddenDesc: "A question that clarifies the reasoning...",
+    miniDesc: "A question that clarifies the reasoning behind a judgement or evaluation made based on certain criteria"
   },
 
   entrepreneur: {
-    flag: 'EN',
-    type: 'Question',
-    name: 'Entrepreneur',
-    nameLower: 'entrepreneur',
-    shortDesc: 'Entrepreneurs use ideas from one domain and apply them in another.',
-    miniDesc: 'A question that takes concepts from one situation and applies them in another'
+    flag: "EN",
+    type: "Question",
+    name: "Entrepreneur",
+    avatar: "avatar avatar-color-13 pull-left",
+    // avatar: "glyphicon glyphicon-question-sign pull-left",
+    char: "?",
+    nameLower: "entrepreneur",
+    shortDesc: "Entrepreneurs use ideas from one domain and apply them in another.",
+    hiddenDesc: "A question that uses concepts from one...",
+    miniDesc: "A question that takes concepts from one situation and applies them in another"
   },
 
   reporter: {
-    flag: 'RP',
-    type: 'Belief',
-    name: 'Reporter',
-    nameLower: 'reporter',
-    shortDesc: 'Reporters provide accurate and verifiable facts.',
-    miniDesc: 'A belief that makes factual claims which can be verified through investigation'
+    flag: "RP",
+    type: "Belief",
+    name: "Reporter",
+    avatar: "avatar avatar-color-81 pull-left",
+    // avatar: "glyphicon glyphicon-exclamation-sign pull-left",
+    char: "!",
+    nameLower: "reporter",
+    shortDesc: "Reporters provide accurate and verifiable facts.",
+    hiddenDesc: "A belief that makes factual claims...",
+    miniDesc: "A belief that makes factual claims which can be verified through investigation"
   },
 
   explorer: {
-    flag: 'EX',
-    type: 'Belief',
-    name: 'Explorer',
-    nameLower: 'explorer',
-    shortDesc: 'Explorers make inferences based on facts.',
-    miniDesc: 'A belief that is based on facts but makes claims about what is not currently known'
+    flag: "EX",
+    type: "Belief",
+    name: "Explorer",
+    avatar: "avatar avatar-color-81 pull-left",
+    // avatar: "glyphicon glyphicon-exclamation-sign pull-left",
+    char: "!",
+    nameLower: "explorer",
+    shortDesc: "Explorers make inferences based on facts.",
+    hiddenDesc: "A belief that is based on facts but...",
+    miniDesc: "A belief that is based on facts but makes claims about what is not currently known"
   },
 
   gateKeeper: {
-    flag: 'GK',
-    type: 'Belief',
-    name: 'Gate Keeper',
-    nameLower: 'gate keeper',
-    shortDesc: 'Gate Keepers render judgements using a relative set of criteria.',
-    miniDesc: 'A belief that expresses an evaluation based on certain criteria'
+    flag: "GK",
+    type: "Belief",
+    name: "Gate Keeper",
+    avatar: "avatar avatar-color-81 pull-left",
+    // avatar: "glyphicon glyphicon-exclamation-sign pull-left",
+    char: "!",
+    nameLower: "gate keeper",
+    shortDesc: "Gate Keepers render judgements using a relative set of criteria.",
+    hiddenDesc: "A belief that expresses an evaluation...",
+    miniDesc: "A belief that expresses an evaluation based on certain criteria"
   }
 }
